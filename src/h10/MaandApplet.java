786a7ee3e589af6e3c53faf3e8af59609605b4d3
Bink-dev <bbavelaar@roc-dev>
2020-10-02 09:28:52 +0200

    H10> Opdracht 10.4 schrikkeljaren werken maar niet op de juiste jaren > hulpvraag ingediend en finetunen.

--- src/h10/MaandApplet.java
+++ src/h10/MaandApplet.java
@@ -7,22 +7,28 @@ import java.awt.event.ActionListener;
 
 public class MaandApplet extends Applet {
     // Variabelen voor Textfield(s)
-    TextField tekstvak;
+    TextField tekstvak1,tekstvak2;
 
     // Variabelen voor String(s)
-    String maand;
+    String output;
 
     // Variabelen voor Int(s)
-    int januari,februari,maart,april,mei,juni,juli,augustus,september,oktober,november,december;
+    int januari,februari,maart,april,mei,juni,juli,augustus,september,oktober,november,december,jaartal;
 
     public void init() {
-        setSize(400,100);
-       tekstvak = new TextField("Vul hier je maandgetal in!");
-       tekstvak.addActionListener(new TekstVakListener());
-       add(tekstvak);
-       maand = "...";
+        // Grootte Applet bepalen
+        setSize(600,200);
 
-       // declareren waardes van maanden (dagaantal)
+        // Textfields declareren
+       tekstvak1 = new TextField("Vul hier een maandgetal in!");
+       tekstvak1.addActionListener(new TekstVakListener());
+       add(tekstvak1);
+       tekstvak2 = new TextField("vul hier een jaartal in!");
+       tekstvak2.addActionListener(new TekstVakListener());
+       add(tekstvak2);
+       output = "...";
+
+       // Declareren waardes van maanden (dagaantal)
         januari = 31;
         februari = 28;
         maart = 31;
@@ -35,34 +41,42 @@ public class MaandApplet extends Applet {
         oktober = 31;
         november = 30;
         december = 31;
+
     }
 
     public void paint(Graphics g) {
-        g.drawString(maand,70,75);
-
+        g.drawString(output,100,75);
     }
 
     class TekstVakListener implements ActionListener{
 
         public void actionPerformed(ActionEvent e) {
-            String userInput = tekstvak.getText();
+            String userInputMaand = tekstvak1.getText();
+            String userInputJaar = tekstvak2.getText();
+            jaartal = Integer.parseInt(userInputJaar);
 
-            switch (userInput){
-                case "1": maand = "Januari " + januari + " dagen."; break;
-                case "2": maand = "Februari " + februari + " dagen. Let op in een schrikkeljaar 29!"; break;
-                case "3": maand = "Maart " + maart + " dagen."; break;
-                case "4": maand = "April " + april + " dagen."; break;
-                case "5": maand = "Mei " + mei + " dagen."; break;
-                case "6": maand = "Juni " + juni + " dagen."; break;
-                case "7": maand = "Juli " + juli + " dagen."; break;
-                case "8": maand = "Augustus " + augustus + " dagen."; break;
-                case "9": maand = "September " + september + " dagen."; break;
-                case "10": maand = "Oktober" + oktober + " dagen."; break;
-                case "11": maand = "November " + november + " dagen."; break;
-                case "12": maand = "December " + december + " dagen."; break;
-                default: maand = "Een echte maandnummer ;)"; break;
-            }
+            switch (userInputMaand){
+                case "1": output = "Januari " + jaartal + " " + januari + " dagen."; break;
+                case "2": output = "Februari " + jaartal + " " + februari + " dagen."; break;
+                case "3": output = "Maart " + jaartal + " " + maart + " dagen."; break;
+                case "4": output = "April " + jaartal + " " + april + " dagen."; break;
+                case "5": output = "Mei " + jaartal + " " + mei + " dagen."; break;
+                case "6": output = "Juni " + jaartal + " " + juni + " dagen."; break;
+                case "7": output = "Juli " + jaartal + " " + juli + " dagen."; break;
+                case "8": output = "Augustus " + jaartal + " " + augustus + " dagen."; break;
+                case "9": output = "September " + jaartal + " " + september + " dagen."; break;
+                case "10": output = "Oktober" + jaartal + " " + oktober + " dagen."; break;
+                case "11": output = "November " + jaartal + " " + november + " dagen."; break;
+                case "12": output = "December " + jaartal + " " + december + " dagen."; break;
+                default: output = "Een echt maandnummer ;)"; break;
+
+            } repaint();
+
+            if ( (jaartal % 4 == 0 && !(jaartal % 100 == 0)) || jaartal % 400 == 0 )
+                februari++;
+            else {februari = 28;}
             repaint();
+            }
         }
     }
-}
+
