c367eb096ff6f8276e9ce28173be3139b2160ff6
Bink-dev <bbavelaar@roc-dev>
2020-10-14 12:46:31 +0200

    H12 > Oefeningen en begin opdracht.

--- src/h12/oefening.java
+++ src/h12/oefening.java
@@ -1,6 +1,19 @@
 package h12;
 
 import java.applet.Applet;
+import java.awt.*;
+import java.util.Arrays;
 
 public class oefening extends Applet {
+   String[] namen = { "Valerie", "Jeroen", "Sander", "Frank"};
+
+    public void init() {
+        Arrays.sort(namen);
+    }
+
+    public void paint(Graphics g) {
+        for (int teller = 0; teller < namen.length; teller++){
+            g.drawString("" + namen[teller],50,20 * teller + 20);
+        }
+    }
 }
