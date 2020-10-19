a13a00b333ed0ef96fe2daa84f958b7c9b017788
Bink-dev <bbavelaar@roc-dev>
2020-10-19 09:20:45 +0200

    H12 > opdracht 1 bekeken met Jorn, gemiddelde word nu berekent.

--- src/h12/GemiddeldeTabel.java
+++ src/h12/GemiddeldeTabel.java
@@ -1,4 +1,28 @@
 package h12;
 
-public class GemiddeldeTabel {
+import java.applet.Applet;
+import java.awt.*;
+
+public class GemiddeldeTabel extends Applet {
+    Double[] cijfers = {7.9, 8.6, 4.9, 6.9, 6.8, 6.7, 7.3, 7.8, 8.1, 8.5};
+
+    double totaal, gemiddelde;
+    int tempGemiddelde;
+
+    public void init() {
+
+    }
+
+    public void paint(Graphics g) {
+        for (int teller = 0; teller < cijfers.length; teller++){
+            g.drawString("" + cijfers[teller],50,25 * teller + 20);
+            totaal += cijfers[teller];
+        }
+        gemiddelde = totaal / 10.0 * 10;
+        tempGemiddelde = (int) gemiddelde;
+        gemiddelde = tempGemiddelde / 10.0;
+
+        g.drawString("Je gemiddelde is = " + gemiddelde,120,140);
+
+    }
 }
