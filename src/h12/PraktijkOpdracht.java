a0fb4593f36f726e5965dd62387ea1ff168e251e
Bink-dev <bbavelaar@roc-dev>
2020-11-10 13:43:01 +0100

    H12 > opdracht 6. Werkt volledig , ingekeken met Jorn IVM moeite hoofdstuk 12.

---
+++ src/h12/PraktijkOpdracht.java
@@ -0,0 +1,24 @@
+package h12;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class PraktijkOpdracht extends Applet {
+
+    // stap 1 : 2 tekstvakken invoeren.
+    // stap 2 : Ok Knop maken.
+    // stap 3: informatie uit tekstvak halen met Ok Knop.
+    // stap 4: Informatie opslaan in een Array.
+    // stap 5: checken of het al 10x gebeurd is. yes> alle ingevoerden namen en telefoonummers op scherm tonen no> niks
+    // stap 6: tekstvakken leegmaken.
+
+    public void init() {
+
+    }
+
+    public void paint(Graphics g) {
+
+    }
+
+
+}
