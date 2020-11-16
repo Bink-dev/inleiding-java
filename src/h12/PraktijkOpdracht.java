a5405268c7dbdf7a43e96917b9c746d297933aa0
Bink-dev <bbavelaar@roc-dev>
2020-11-16 10:47:59 +0100

    H12 > praktijkopdracht systeem werkt volledig.

--- src/h12/PraktijkOpdracht.java
+++ src/h12/PraktijkOpdracht.java
@@ -7,127 +7,129 @@ import java.awt.event.ActionListener;
 
 public class PraktijkOpdracht extends Applet {
 
-    // stap 1 : 2 tekstvakken invoeren.
-    // stap 2 : Ok Knop maken.
-    // stap 3: informatie uit tekstvak halen met Ok Knop.
-    // stap 4: Informatie opslaan in een Array.
-    // stap 5: checken of het al 10x gebeurd is. yes> alle ingevoerden namen en telefoonummers op scherm tonen no> niks
-    // stap 6: tekstvakken leegmaken.
+    TextField textFieldName, textFieldNumber;
 
-    TextField tekstVakNaam, tekstVakNummer;
+    Label name, phoneNumber;
 
-    Label naam, telefoonNummer;
+    Button okButton;
 
-    Button okKnop;
+    String[] nameArray = new String[10];
+    String userInputName1, userInputName2, userInputName3, userInputName4, userInputName5, userInputName6, userInputName7, userInputName8, userInputName9, userInputName10;
 
-    String[] naamArray = new String[10];
-    String userInputNaam1,userInputNaam2,userInputNaam3,userInputNaam4,userInputNaam5,userInputNaam6,userInputNaam7,userInputNaam8,userInputNaam9,userInputNaam10;
+    int[] numberArray = new int[10];
+    int userInputNumber1, userInputNumber2, userInputNumber3, userInputNumber4, userInputNumber5, userInputNumber6, userInputNumber7, userInputNumber8, userInputNumber9, userInputNumber10;
 
-    int[] nummerArray = new int[10];
-    int userInputNummer1,userInputNummer2,userInputNummer3,userInputNummer4,userInputNummer5,userInputNummer6,userInputNummer7,userInputNummer8,userInputNummer9,userInputNummer10;
+    int globalCounter = 0;
 
     public void init() {
-        tekstVakNaam = new TextField("", 4);
-        tekstVakNummer = new TextField("", 10);
-        naam = new Label("Naam :");
-        telefoonNummer = new Label("Telefoonnummer :");
-        okKnop = new Button("Ok");
-        okKnop.addActionListener(new OkKnopListener());
 
-        add(naam);
-        add(tekstVakNaam);
-        add(telefoonNummer);
-        add(tekstVakNummer);
-        add(okKnop);
+        textFieldName = new TextField("", 4);
+        textFieldNumber = new TextField("", 10);
+        name = new Label("Name :");
+        phoneNumber = new Label("Telephone Number :");
+        okButton = new Button("Ok");
+        okButton.addActionListener(new OkButtonListener());
+
+        add(name);
+        add(textFieldName);
+        add(phoneNumber);
+        add(textFieldNumber);
+        add(okButton);
     }
 
     public void paint(Graphics g) {
-        for (int teller = 0; teller < naamArray.length && teller < nummerArray.length; teller++){
-            if (teller <= 10){
-                g.drawString("" + naamArray[teller],70,60 + teller * 25);
-                g.drawString("" + nummerArray[teller],200,60 + teller * 25);
+
+        for (int counter = 0; counter < nameArray.length && counter < numberArray.length; counter++){
+
+            if (globalCounter == 10){
+                System.out.println(counter);
+                g.drawString("" + nameArray[counter],70,60 + counter * 25);
+                g.drawString("" + numberArray[counter],200,60 + counter * 25);
 
             }
         }
     }
 
-    class OkKnopListener implements ActionListener{
+    class OkButtonListener implements ActionListener{
 
         public void actionPerformed(ActionEvent e) {
 
-            naamArray[0] = userInputNaam1;
-            naamArray[1] = userInputNaam2;
-            naamArray[2] = userInputNaam3;
-            naamArray[3] = userInputNaam4;
-            naamArray[4] = userInputNaam5;
-            naamArray[5] = userInputNaam6;
-            naamArray[6] = userInputNaam7;
-            naamArray[7] = userInputNaam8;
-            naamArray[8] = userInputNaam9;
-            naamArray[9] = userInputNaam10;
-
-            nummerArray[0] = userInputNummer1;
-            nummerArray[1] = userInputNummer2;
-            nummerArray[2] = userInputNummer3;
-            nummerArray[3] = userInputNummer4;
-            nummerArray[4] = userInputNummer5;
-            nummerArray[5] = userInputNummer6;
-            nummerArray[6] = userInputNummer7;
-            nummerArray[7] = userInputNummer8;
-            nummerArray[8] = userInputNummer9;
-            nummerArray[9] = userInputNummer10;
-
-            for (int teller = 0; teller < naamArray.length && teller < nummerArray.length; teller++){
-                if (teller == 0){
-                    userInputNaam1 = tekstVakNaam.getText();
-                    userInputNummer1 = Integer.parseInt(tekstVakNummer.getText());
+                if (globalCounter == 0){
+                    userInputName1 = textFieldName.getText();
+                    userInputNumber1 = Integer.parseInt(textFieldNumber.getText());
                 }
 
-                if (teller == 1){
-                    userInputNaam2 = tekstVakNaam.getText();
-                    userInputNummer2 = Integer.parseInt(tekstVakNummer.getText());
+                if (globalCounter == 1){
+                    userInputName2 = textFieldName.getText();
+                    userInputNumber2 = Integer.parseInt(textFieldNumber.getText());
                 }
 
-                if (teller == 2){
-                    userInputNaam3 = tekstVakNaam.getText();
-                    userInputNummer3 = Integer.parseInt(tekstVakNummer.getText());
+                if (globalCounter == 2){
+                    userInputName3 = textFieldName.getText();
+                    userInputNumber3 = Integer.parseInt(textFieldNumber.getText());
                 }
 
-                if (teller == 3){
-                    userInputNaam4 = tekstVakNaam.getText();
-                    userInputNummer4 = Integer.parseInt(tekstVakNummer.getText());
+                if (globalCounter == 3){
+                    userInputName4 = textFieldName.getText();
+                    userInputNumber4 = Integer.parseInt(textFieldNumber.getText());
                 }
 
-                if (teller == 4){
-                    userInputNaam5 = tekstVakNaam.getText();
-                    userInputNummer5 = Integer.parseInt(tekstVakNummer.getText());
+                if (globalCounter == 4){
+                    userInputName5 = textFieldName.getText();
+                    userInputNumber5 = Integer.parseInt(textFieldNumber.getText());
                 }
 
-                if (teller == 5){
-                    userInputNaam6 = tekstVakNaam.getText();
-                    userInputNummer6 = Integer.parseInt(tekstVakNummer.getText());
+                if (globalCounter == 5){
+                    userInputName6 = textFieldName.getText();
+                    userInputNumber6 = Integer.parseInt(textFieldNumber.getText());
                 }
 
-                if (teller == 6){
-                    userInputNaam7 = tekstVakNaam.getText();
-                    userInputNummer7 = Integer.parseInt(tekstVakNummer.getText());
+                if (globalCounter == 6){
+                    userInputName7 = textFieldName.getText();
+                    userInputNumber7 = Integer.parseInt(textFieldNumber.getText());
                 }
 
-                if (teller == 7){
-                    userInputNaam8 = tekstVakNaam.getText();
-                    userInputNummer8 = Integer.parseInt(tekstVakNummer.getText());
+                if (globalCounter == 7){
+                    userInputName8 = textFieldName.getText();
+                    userInputNumber8 = Integer.parseInt(textFieldNumber.getText());
                 }
 
-                if (teller == 8){
-                    userInputNaam9 = tekstVakNaam.getText();
-                    userInputNummer9 = Integer.parseInt(tekstVakNummer.getText());
+                if (globalCounter == 8){
+                    userInputName9 = textFieldName.getText();
+                    userInputNumber9 = Integer.parseInt(textFieldNumber.getText());
                 }
 
-                if (teller == 9){
-                    userInputNaam10 = tekstVakNaam.getText();
-                    userInputNummer10 = Integer.parseInt(tekstVakNummer.getText());
+                if (globalCounter == 9){
+                    userInputName10 = textFieldName.getText();
+                    userInputNumber10 = Integer.parseInt(textFieldNumber.getText());
                 }
-            }
+
+                globalCounter++;
+                textFieldNumber.setText("");
+                textFieldName.setText("");
+
+                nameArray[0] = userInputName1;
+                nameArray[1] = userInputName2;
+                nameArray[2] = userInputName3;
+                nameArray[3] = userInputName4;
+                nameArray[4] = userInputName5;
+                nameArray[5] = userInputName6;
+                nameArray[6] = userInputName7;
+                nameArray[7] = userInputName8;
+                nameArray[8] = userInputName9;
+                nameArray[9] = userInputName10;
+
+                numberArray[0] = userInputNumber1;
+                numberArray[1] = userInputNumber2;
+                numberArray[2] = userInputNumber3;
+                numberArray[3] = userInputNumber4;
+                numberArray[4] = userInputNumber5;
+                numberArray[5] = userInputNumber6;
+                numberArray[6] = userInputNumber7;
+                numberArray[7] = userInputNumber8;
+                numberArray[8] = userInputNumber9;
+                numberArray[9] = userInputNumber10;
+
             repaint();
         }
     }
